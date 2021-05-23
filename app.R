1db9e29e3ab40153815b43b1d33403e4e452a9dc
Bilal1k <48079226+Bilal1k@users.noreply.github.com>
2021-05-23 12:26:32 -0400

    workeing new version

--- app.R
+++ app.R
@@ -5,104 +5,101 @@ library(leaflet)
 library(rgdal)
 library(stringr)
 library(sf)
+library(shinyalert)
+library(wrapr)
 
-minor_geom <- st_read("minor_geom.shp")
+source("leaflet.R")
+
+minor_geom <- st_read("min_geom.shp")
 minor_df <- readRDS("minor_df.rds")
-minor_df <- st_set_geometry(minor_df, value = minor_geom$geometry)
+minor_geom <- st_set_geometry(minor_df, value = minor_geom$geometry)
 minor_nm <- readRDS("minor_nm.rds")
 
 
 POB_geom <- st_read("POB_geom.shp")
 POB_df <- readRDS("POB_df.rds")
-POB_df <-  st_set_geometry(POB_df, value = POB_geom$geometry)
-con_nm <-readRDS("con_nm.rds")
+POB_geom <- st_set_geometry(POB_df, value = POB_geom$geometry)
+con_nm <- readRDS("con_nm.rds")
 
-Google_template <- 
-    "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga"
-
+POB_geom_rec <- st_read("POB_geom_rec.shp")
+POB_df_rec <- readRDS("POB_df_rec.rds")
+POB_geom_rec <- st_set_geometry(POB_df_rec, value = POB_geom_rec$geometry)
+con_nm_rec <- readRDS("con_nm_rec.rds")
 
 
 ui <- fluidPage(
-    titlePanel("Chloropleth of Visible Minorities and Places of Birth for the GTA Population"),
-    
-    sidebarLayout(
-        sidebarPanel(
-            helpText("Based on the 2016 census."), width = 4,
-            helpText( "Due to the large amount of data, it will take about 10 seconds to load each map"),
-          
+
+    titlePanel("Chloropleth of Census Data for the GTA Population"),
+  
+        sidebarPanel(width = 12,
+        actionButton("i", "info"),
+                     
+                     
           selectInput("var",
-                      label = "Choose a Variable",
+                      label = "Choose a variable",
                       choices = c("Visible minority",
-                                  "Place of birth"),
+                                  "Immigrants places of birth",
+                                  "Recent immigrants places of birth"),
                       selected = "Visible minority"),
           
           conditionalPanel(condition = "input.var == 'Visible minority'",
-            selectInput("subset", 
+            selectInput("minor", 
                         label = "Choose a minority",
                         choices = minor_nm,
-                        selected = "South Asian")),
+                       )),
             
-            conditionalPanel(condition = "input.var == 'Place of birth for immigrant population'",
-                             selectInput("subset", 
-                                         label = "Choose a country",
-                                         choices = con_nm,
-                                         selected = "United States")),
-            helpText( "Click on dissimination area to get percentage and geocode")
-
+          conditionalPanel(condition = "input.var == 'Immigrants places of birth'",
+             selectInput("pob", 
+                         label = "Choose a place",
+                         choices = con_nm,
+                         )),
+        
+        conditionalPanel(condition = "input.var == 'Recent immigrants places of birth'",
+                         selectInput("pob_rec",
+                                     label = "Choose a place",
+                                     choices = con_nm_rec,
+                         )),
         ),
         
-        mainPanel(leafletOutput("map"), width = 12
+        mainPanel(leafletOutput("map"), width = 12)
 )
-    )
-)
+    
 
 
 server <- function(input, output){
-
-      output$map <- renderLeaflet({
-      
-        
-        data < switch(input$var,
-                      "Visible minority" = minor_df,
-                      "Place of birth" = POB_df)
-          
-         mino <- input$data$sub 
-                        
-                 # "South Asian" = data$SthsA,
-                 # "Chinese"= data$Chins,
-                 # "Black" = data$Black_1,
-                 # "Filipino" = data$Filpn,
-                 # "Latin American" = data$LtnAm,
-                 # "Arab" = data$Arab,
-                 # "Southeast Asian" = data$SthsA,
-                 # "West Asian" = data$WstAs,
-                 # "Korean" = data$Koren,
-                 # "Japanese" = data$Japns)
-         
-         name <- switch(input$var, 
-                        "South Asian" = "South Asians",
-                        "Chinese" = "Chinese",
-                        "Black" = "Blacks",
-                        "Filipino" = "Filipinos",
-                        "Latin American" = "Latin Americans",
-                        "Arab" = "Arabs",
-                        "Southeast Asian" = "Southeast Asians",
-                        "West Asian" = "West Asians",
-                        "Korean" = "Koreans",
-                        "Japanese" = "Japanese")
-          
-          pal <- colorNumeric(
-              palette = "Reds",
-              domain = mino)
-          
-          withProgress(message = 'Making plot', value = 1, {
-          
-        mymap(data, sub)
-                     
-          
-        })
-      })
+        observeEvent(input$i, {
+          showNotification("Based on the 2016 census. 
+                   Recent immigrant refers to a person who obtained a
+                   landed immigrant or permanent resident status up to five
+                     years prior to a given census year" , type = "message")}
+        )
+            
     
-}
-# Run app ----
+        output$map <- renderLeaflet({
+       
+          
+        dat <-  switch(input$var,
+                   "Visible minority" = minor_df,
+                   "Immigrants places of birth" = POB_df,
+                   "Recent immigrants places of birth" = POB_df_rec)
+          
+        geom <- switch(input$var,
+                        "Visible minority" = minor_geom,
+                        "Immigrants places of birth" = POB_geom,
+                       "Recent immigrants places of birth" = POB_geom_rec
+                       )
+        
+        sub <-  switch(input$var,
+                       "Visible minority" = input$minor,
+                       "Immigrants places of birth" = input$pob,
+                       "Recent immigrants places of birth" = input$pob_rec)
+        
+
+        mymap(dat,sub,geom)
+    
+        })}
+
+# Run app 
 shinyApp(ui, server)
+
+
---
+++ leaflet.R
@@ -0,0 +1,33 @@
+library(leaflet)
+library(rgdal)
+library(stringr)
+library(sf)
+
+mymap <- function(df, sub, geom){
+
+  Google_template <-
+    "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga"
+  
+  pal <- colorNumeric(
+  palette = "Reds",
+  domain = df[,sub])
+
+  c <- st_bbox(geom)
+  leaflet(data = geom, options = leafletOptions(minZoom = 9, maxZoom = 18)) %>%
+    
+    setMaxBounds(c[[1]],c[[2]],c[[3]],c[[4]]) %>%
+
+    # Choose map type
+    addTiles(urlTemplate = Google_template, attribution = 'Google, Bilal') %>%
+    
+    # add polygons and legend
+    addPolygons(opacity = 0.3, weight = 0.3, fillOpacity = 0.6, color = "Gray",
+                fillColor = ~pal(df[,sub]),
+                popup = str_c(as.character(df[,sub]),
+                              df[,"GeoUID"],
+                              sep = "% - DA: ")) %>%
+    addLegend("bottomright", pal = pal, values = ~df[,sub],
+              title = "Percentage", opacity = 0.3)
+}
+
+
